8779664139bb86c775927437852d8eee2715ce7b
Dmitry Kh <dnhaziev@mail.ru>
2024-04-24 13:41:20 +0500

    Complite

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         String[] name = { "Иван Иванов", "Николай Сидоров", "Петр Петров", "Алексей Кузнецов", "Сергей Сергеев",
-                "Анна Болотникова", "Татьяна Кретова", "Светлана Светикова", "Вероника Козлова" }; // 9
+                "Анна Болотникова", "Татьяна Кретова", "Светлана Светикова", "Вероника Козлова" };
 
         Random phone = new Random();
         Random nameRnd = new Random();
@@ -17,8 +23,10 @@ public class Main {
             myPhoneBook.add(name[nameRnd.nextInt(9)], phone.nextInt(999_999 - 100_000 + 1) + 100_000);
         }
 
-        System.out.println(myPhoneBook.find("Иван Иванов"));
-        System.out.println(PhoneBook.getPhoneBook());
+        System.out.println(PhoneBook.getPhoneBook()); //Для сравнения не отсортированный
+        System.out.println("-----------------------------------------------------");
+        myPhoneBook.printPhone(); //Отсортированный справочник
+        
 
     }
 
@@ -33,9 +41,9 @@ public class Main {
             }
             phoneBook.get(name).add(phoneNum);
 
-        }
+        }     
 
-        public ArrayList<Integer> find(String name) {
+		public ArrayList<Integer> find(String name) {
 
             if (!phoneBook.containsKey(name)) {
                 return new ArrayList<>();
@@ -44,11 +52,33 @@ public class Main {
             }
 
         }
-
+        //Метод только для сравнения отсортированного и нет
+        
         public static HashMap<String, ArrayList<Integer>> getPhoneBook() {
 
             return phoneBook;
         }
+
+       
+
+        public  void printPhone (){
+            ArrayList<String> phoneList = new ArrayList<>();
+            for (Map.Entry<String, ArrayList<Integer>> entry: phoneBook.entrySet()){
+                String key = entry.getKey();
+                ArrayList <Integer> value = entry.getValue();
+                phoneList.add(key + " = " + value);
+                        }
+            Comparator<String> stringLen = new StringLenSort(); 
+            Collections.sort(phoneList, stringLen);
+            for (String item: phoneList){
+                System.out.println(item);
+            }
+            
+            
+        }
     }
 
+    
+
 }
+
